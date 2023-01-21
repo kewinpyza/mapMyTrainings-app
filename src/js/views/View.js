842d93289ecf8b541e26cd8d8a6489976e2e0f1e
kewinpyza <kewin.pyza@gmail.com>
2023-01-21 20:55:08 +0100

    Complete displaying error window

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -5,21 +5,23 @@ export default class View {
   _windowErr = document.querySelector('.error-window');
   _textErr = document.querySelector('.error-window__text');
   _overlay = document.querySelector('.overlay');
-  _btnErr = document.querySelector('error-window__btn-close');
+  _btnErr = document.querySelector('.error-window__btn-close');
 
   renderError(errorMsg) {
     this._textErr.innerHTML = errorMsg;
-    this._toggleWindow();
     this._closeWindow();
+    this._toggleError();
   }
 
-  _toggleWindow() {
-    this._windowErr.classList.toggle('hidden');
-    this._overlay.classList.toggle('hidden');
+  _toggleError() {
+    [this._windowErr, this._overlay].forEach(el =>
+      el.classList.toggle('hidden')
+    );
   }
 
   _closeWindow() {
-    this._btnErr.addEventListener('click', this._toggleWindow.bind(this));
-    this._overlay.addEventListener('click', this._toggleWindow.bind(this));
+    [this._btnErr, this._overlay].forEach(closer =>
+      closer.addEventListener('click', this._toggleError.bind(this))
+    );
   }
 }
